3461e5fc4ba9d25796b8640ee268515da4a29258
Jaaum32 <jv418070@gmail.com>
2023-05-15 20:54:52 -0300

    Controllers

---
+++ Controller/ItemPedidoRepositoryMySQL.cs
@@ -0,0 +1,32 @@
+using Gamification03.Interfaces;
+using Gamification03.Model;
+
+namespace Gamification03.Controller;
+
+public class ItemPedidoRepositoryMySQL : IItemPedidoRepository
+{
+    public void Adicionar(ItemPedido itemPedido)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Atualizar(ItemPedido itemPedido)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Excluir(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemPedido ObterPorId(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<ItemPedido> ListarTodos()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Controller/PedidoRepositoryMySQL.cs
@@ -0,0 +1,32 @@
+using Gamification03.Interfaces;
+using Gamification03.Model;
+
+namespace Gamification03.Controller;
+
+public class PedidoRepositoryMySQL : IPedidoRepository
+{
+    public void Adicionar(Pedido pedido)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Atualizar(Pedido pedido)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Excluir(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Pedido ObterPorId(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Pedido> ListarTodos()
+    {
+        throw new NotImplementedException();
+    }
+}
